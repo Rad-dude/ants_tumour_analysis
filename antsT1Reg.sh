0d8cc0eabd86f98c6e8e554296dcdb29a6699717
Michael Hart <mgh40@cam.ac.uk>
2016-04-18 13:46:12 +0100

    Added files via upload

---
+++ antsT1Reg.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+#Michael Hart, University of Cambridge, 13 April 2016 (c)
+
+usage()
+{
+cat<<EOF
+usage: $0 options
+
+===========================================================================
+
+antsT1Reg.sh
+
+(c) Michael Hart, University of Cambridge, 2016
+
+Registers a T1 image to standard space (e.g. MNI)
+
+Example:
+
+bash antsT1Reg.sh -a mprage_brain.nii.gz -t MNI152_T1_2mm.nii.gz
+
+Options:
+
+-h  show this help
+-a  anatomical (moving image)
+-t  skull stripped template (fixed image)
+
+============================================================================
+
+EOF
+}
+
+#initialise options
+
+while getopts "ha:t:m" OPTION
+do
+    case $OPTION in
+    h)
+        usage
+        exit 1
+        ;;
+    a)
+        usage
+        anat=$OPTARG
+        ;;
+    t)
+        template=$OPTARG
+        ;;
+    ?)
+        usage
+        exit
+        ;;
+    esac
+done
+
+#check usage
+
+if [[ -z $anat ]] || [[ -z $template ]] || [[ -z $mask ]]
+then
+    usage
+    exit 1
+fi
+
+echo "files and options ok"
+
+#Create registration
+bash antsRegistrationSyN.sh \
+-d 3 \
+-m $anat \
+-f $template \
+-o AR_
+
+#Apply transforms
+antsApplyTransforms
+-d 3
+-i $anat
+-o AT_
+-r $template
+-t AR_affine0
+-t AR_diff1warp.nii.gz
+
+#Quality control output
+slices $template $anat ANTS_T1Reg_check.gif
