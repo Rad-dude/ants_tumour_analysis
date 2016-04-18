ba9c5845df064e5de1157460b236839f0a622963
Michael Hart <mgh40@cam.ac.uk>
2016-04-18 10:53:09 +0100

    Added files via upload
    antsBrains.sh now added

---
+++ antsBrains.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#Michael Hart, University of Cambridge, 13 April 2016 (c)
+
+usage()
+{
+cat<<EOF
+usage: $0 options
+
+===========================================================================
+
+antsBrains.sh
+
+(c) Michael Hart, University of Cambridge, 2016
+
+Does brain extraction with ANTs
+
+Example:
+
+bash antsBrains.sh -a mprage.nii.gz -t MNI152_T1_2mm.nii.gz -m MNI152_T1_2mm_brain_mask.nii.gz
+
+Options:
+
+-h  show this help
+-a  anatomical
+-t  template (with skull)
+-m  template brain mask
+
+============================================================================
+
+EOF
+}
+
+#initialise options
+
+while getopts "h:a:t:m" OPTION
+do
+    case $OPTION in
+    h)
+        usage
+        exit 1
+        ;;
+    a)
+        anat=$OPTARG
+        ;;
+    t)
+        template=$OPTARG
+        ;;
+    m)
+        mask=$OPTARG
+        ;;
+    ?)
+        usage
+        exit
+        ;;
+    esac
+done
+
+#check usage
+
+if [[ -z $anat ]] || [[ -z $template ]] || [[ -z $mask ]]
+then
+    usage
+    exit 1
+fi
+
+echo "files and options ok"
+
+bash antsBrainExtraction.sh \
+-d 3 \
+-a $anat \
+-e $template \
+-m $mask \
+-o ABE_
